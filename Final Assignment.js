f5ac4f71ed804a9d8762e16a7788437005a3fc70
shigi66 <hatsumicortezgmail.com>
2025-04-08 16:04:24 -0300

    correcion

--- Final Assignment.js
+++ Final Assignment.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.lista img').click(function () {
-    const imagenSeleccionada = $(this).data('ulmo');
+    const imagenSeleccionada = $(this).data('ulmo');  
 
     $('.ulmi-dog img').fadeOut( function () {
       $(this).attr('src', imagenSeleccionada).fadeIn(200);
